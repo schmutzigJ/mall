838c40c6e97abd069306eb558c60534828e34b58
schmutzigJ <770930759@qq.com>
2022-02-20 17:42:17 +0800

    首页导航

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,8 +1,22 @@
 <template>
-  <div>Homes</div>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">商品</div>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
-export default {}
+import navBar from '../../components/common/navbar/navBar.vue'
+export default {
+  components: { navBar }
+}
 </script>
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+#home {
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #fff;
+  }
+}
+</style>
